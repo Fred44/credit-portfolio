9e6644ea8ba4961bb0b60d78ac6bc01d921eed69
Frederic LEFEBVRE <flefebvre44@gmail.com>
2015-10-21 22:10:55 +0200

    Number format, it catches parse exception and displays error

--- app/utils/FormFieldImplicits.scala
+++ app/utils/FormFieldImplicits.scala
@@ -3,8 +3,8 @@ package utils
 import java.text.NumberFormat
 import java.util.Locale
 
-import play.api.data.Forms
-import play.api.data.format.Formatter
+import play.api.data.{FormError, Forms}
+import play.api.data.format.{Formats, Formatter}
 
 /**
  * Created by fred on 29/09/15.
@@ -14,7 +14,26 @@ object FormFieldImplicits {
   private val fmt = NumberFormat.getNumberInstance(Locale.FRANCE)
 
   implicit def bigDecimalFmt = new Formatter[BigDecimal] {
-    def bind(key: String, data: Map[String, String]) = Right(BigDecimal(fmt.parse(data(key).replace('.', ',')).doubleValue()))
+    def bind(key: String, data: Map[String, String]) = {
+      Formats.stringFormat.bind(key, data).right.flatMap { s =>
+        scala.util.control.Exception.allCatch[BigDecimal]
+          .either {
+            val bd = BigDecimal(fmt.parse(s.replace('.', ',')).doubleValue())
+            if (bd.scale > 2) {
+              throw new java.lang.ArithmeticException("Invalid precision")
+            }
+            bd
+          }
+          .left.map { e =>
+            Seq(
+              e match {
+                case _:java.lang.ArithmeticException => FormError(key, "error.real.precision", Seq(2))
+                case _ => FormError(key, "error.real", Nil)
+              }
+            )
+          }
+      }
+    }
     def unbind(key: String, value: BigDecimal) = Map(key -> fmt.format(value))
   }
 
